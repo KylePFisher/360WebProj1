e6c6567c2dffd1116fcbb8e2cd501edacf459938
Kyle Fisher <kpfisher@mail.mccneb.edu>
2017-01-09 19:01:27 +0000

    Completed verification for contact us page, using validateContact function in validation.js

--- Milestone_3/JavaScript/validation.js
+++ Milestone_3/JavaScript/validation.js
@@ -12,16 +12,53 @@ function validateSignIn() {
 }
 
 function validateContact() {
+    var errorParagraphs = document.querySelectorAll(".failedContactSubmission");
+    var successfulSubmission = true;
     var name = document.forms["contactUsForm"]["uName"].value;
     var email = document.forms["contactUsForm"]["email"].value;
     var phone = document.forms["contactUsForm"]["telephone"].value;
     var comments = document.forms["contactUsForm"]["comments"].value;
-    if (name == "" || email == "" || phone == "" || comments == "") {
-        document.getElementById("failedContactSubmission").style.display = "block";
-        return false;
+    
+    var phoneRegex = /^[0-9]{3}-[0-9]{3}-[0-9]{4}$/;
+    var emailRegex = /^[a-z0-9]+@[a-z0-9]+\.[a-z]{3}$/;
+    var commentsRegex = /^.{1,250}$/;
+
+    var i;
+    for (i = 0; i < errorParagraphs.length; i++)
+    {
+        errorParagraphs[i].style.display = "none";
     }
-    else
+    
+    if (name == "" || email == "" || phone == "" || comments == "") {
+        errorParagraphs[0].style.display = "block";
+        successfulSubmission = false;
+    }
+    
+
+    if (!email.match(emailRegex))
+    {
+        errorParagraphs[1].style.display = "block";
+        console.log('email triggered');
+        successfulSubmission = false;
+    }
+
+    if (!phone.match(phoneRegex))
+    {
+        errorParagraphs[2].style.display = "block";
+        successfulSubmission = false;
+    }
+
+    if (!comments.match(commentsRegex))
+    {
+        errorParagraphs[3].style.display = "block";
+        console.log('comments triggered');
+        successfulSubmission = false;
+    }
+
+    if (successfulSubmission)
     {
         return true;
     }
+
+    return false;  
 }
